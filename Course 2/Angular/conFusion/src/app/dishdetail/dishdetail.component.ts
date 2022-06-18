ba5fa76e0d8c66cf8031ea6b13d543c2a43c47ac
Kasup Sri <ksri1987@gmail.com>
2022-06-18 22:41:28 +0800

    routing with params

--- Course 2/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Course 2/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { DishService } from '../services/dish.service';
 
 @Component({
-  selector: "app-dishdetail",
-  templateUrl: "./dishdetail.component.html",
-  styleUrls: ["./dishdetail.component.scss"],
+  selector: 'app-dishdetail',
+  templateUrl: './dishdetail.component.html',
+  styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent implements OnInit {
-  
-  @Input()
-  dish:Dish;
+  dish: Dish;
 
-  constructor() {}
+  constructor(private dishService: DishService, private route: ActivatedRoute, private loation: Location) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    // url /dishdetail/2
+    let id = this.route.snapshot.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
 
+  goBack(): void {
+    this.loation.back();
+  }
 }
