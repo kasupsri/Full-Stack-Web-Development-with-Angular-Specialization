de161a07151aae1994376f64c38b6f83d78da25b
Kasup Sri <ksri1987@gmail.com>
2022-06-21 13:42:13 +0800

    erros fix

--- Course 2/Angular/conFusion/src/app/app.module.ts
+++ Course 2/Angular/conFusion/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatGridListModule } from '@angular/material/Grid-list';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
