a4dc53ddc1a4a75a9a0b97d51d103f07406bda54
Kasup Sri <ksri1987@gmail.com>
2022-06-30 18:04:23 +0800

    assignment complete for week 2

--- Course 3/NodeJS/conFusionServer/app.js
+++ Course 3/NodeJS/conFusionServer/app.js
@@ -4,14 +4,18 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-const mongoose = require("mongoose");
-const Dishes = require("./models/dishes");
+
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+
+const mongoose = require("mongoose");
+const Dishes = require("./models/dishes");
+const Leaders = require("./models/leaders");
+const Promotions = require("./models/promotions");
 
 const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
