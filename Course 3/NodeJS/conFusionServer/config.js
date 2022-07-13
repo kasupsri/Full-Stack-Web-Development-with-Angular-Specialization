2773c495cfa691fc066670a24a3109be7ccc1ea9
Kasup Sri Bandara <ksri1987@gmail.com>
2022-07-13 10:58:46 +0800

    keys removed

--- Course 3/NodeJS/conFusionServer/config.js
+++ Course 3/NodeJS/conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
   facebook: {
-    clientId: "726954291964817",
-    clientSecret: "c4950a3c962a0060cbc705d7dbc2f3c9",
+    clientId: "clientID",
+    clientSecret: "cilentSecret",
   },
 };
